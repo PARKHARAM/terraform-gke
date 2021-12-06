6bf3a289948bb6f7457e5ab8342acea5f0a0abd4
PARKHARAM <33815176+PARKHARAM@users.noreply.github.com>
2021-12-06 23:48:12 +0900

    Update gke.tf

--- gke.tf
+++ gke.tf
@@ -96,13 +96,49 @@ resource "google_container_node_pool" "primary_nodes" {
 # }
 
 
-provider "kubernetes" {
-  config_path    = "~/config"
-  config_context = "my-context"
-}
-
-resource "kubernetes_namespace" "example" {
+resource "kubernetes_pod" "with_pod_affinity" {
   metadata {
-    name = "my-first-namespace"
+    name = "with-pod-affinity"
+  }
+
+  spec {
+    affinity {
+      pod_affinity {
+        required_during_scheduling_ignored_during_execution {
+          label_selector {
+            match_expressions {
+              key      = "security"
+              operator = "In"
+              values   = ["S1"]
+            }
+          }
+
+          topology_key = "failure-domain.beta.kubernetes.io/zone"
+        }
+      }
+
+      pod_anti_affinity {
+        preferred_during_scheduling_ignored_during_execution {
+          weight = 100
+
+          pod_affinity_term {
+            label_selector {
+              match_expressions {
+                key      = "security"
+                operator = "In"
+                values   = ["S2"]
+              }
+            }
+
+            topology_key = "failure-domain.beta.kubernetes.io/zone"
+          }
+        }
+      }
+    }
+
+    container {
+      name  = "with-pod-affinity"
+      image = "k8s.gcr.io/pause:2.0"
+    }
   }
 }
