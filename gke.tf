3a9d5f8f67d59ddecbe0fcf9326d8f323059e902
PARKHARAM <33815176+PARKHARAM@users.noreply.github.com>
2021-12-06 21:54:02 +0900

    Update gke.tf

--- gke.tf
+++ gke.tf
@@ -98,19 +98,19 @@ resource "google_container_node_pool" "primary_nodes" {
 terraform {
 
 }
-provider "kubernetes" {
-  config_path = "/var/lib/jenkins/config"
-}
- #provider "kubernetes" {
-  #load_config_file = "false"
-
-  #host     = google_container_cluster.primary.endpoint
-  #username = var.gke_username
-  #password = var.gke_password
-  #client_certificate     = google_container_cluster.primary.master_auth.0.client_certificate
-  #client_key             = google_container_cluster.primary.master_auth.0.client_key
-  #cluster_ca_certificate = google_container_cluster.primary.master_auth.0.cluster_ca_certificate
+#provider "kubernetes" {
+#  config_path = "/var/lib/jenkins/config"
 #}
+provider "kubernetes" {
+ load_config_file = "false"
+
+ host     = google_container_cluster.primary.endpoint
+ username = var.gke_username
+ password = var.gke_password
+ client_certificate     = google_container_cluster.primary.master_auth.0.client_certificate
+ client_key             = google_container_cluster.primary.master_auth.0.client_key
+ cluster_ca_certificate = google_container_cluster.primary.master_auth.0.cluster_ca_certificate
+}
 resource "kubernetes_namespace" "test" {
   metadata {
     name = "nginx"
