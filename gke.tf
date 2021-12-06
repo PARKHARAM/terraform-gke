f7fc6e3a98949dfe8628766a18ce3411b6efa23e
PARKHARAM <33815176+PARKHARAM@users.noreply.github.com>
2021-12-06 21:13:28 +0900

    Update gke.tf

--- gke.tf
+++ gke.tf
@@ -96,12 +96,7 @@ resource "google_container_node_pool" "primary_nodes" {
 # }
 
 terraform {
-  required_providers {
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = ">= 2.0.0"
-    }
-  }
+
 }
 provider "kubernetes" {
   config_path = "~/.kube/config"
