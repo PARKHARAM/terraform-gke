fa45e1598ca2b412d849675e8a0a87d20408e123
PARKHARAM <33815176+PARKHARAM@users.noreply.github.com>
2021-12-06 21:15:37 +0900

    Update gke.tf

--- gke.tf
+++ gke.tf
@@ -101,10 +101,15 @@ terraform {
 provider "kubernetes" {
   config_path = "~/.kube/config"
 }
-resource "kubernetes_namespace" "test" {
-  metadata {
-    name = "nginx"
-  }
+provider "kubernetes" {
+  load_config_file = "false"
+
+  host     = google_container_cluster.primary.endpoint
+  username = var.gke_username
+  password = var.gke_password
+  client_certificate     = google_container_cluster.primary.master_auth.0.client_certificate
+  client_key             = google_container_cluster.primary.master_auth.0.client_key
+  cluster_ca_certificate = google_container_cluster.primary.master_auth.0.cluster_ca_certificate
 }
 resource "kubernetes_deployment" "test" {
   metadata {
